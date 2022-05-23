76e1ff2e578e14e1b93ebae7fc2c26f7f065a59e
natigalsan <natividadgalsan@gmail.com>
2022-05-23 19:13:45 +0000

    bloSW API

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,76 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# ----------------------------------------------------------------------------------------------------------------
+# vamos a crear nuevas rutas en este archivo:  PRIMER PUNTO DEL EJERCICIO: 
+@app.route('/people', methods=['GET'])
+def get_people():
+    # conlo siguiente consigo todos los personajes: 
+    allpeople = People.query.all()
+    # print(allpeople[0].serialize())
+
+    allpeople = list(map(lambda elemento: elemento.serialize(), allpeople))
+    # esto es un mapeo en python
+    print(allpeople)
+    return jsonify({"resultado": allpeople})
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    # conlo siguiente consigo todos los personajes: 
+    allplanets = Planets.query.all()
+    allplanets = list(map(lambda elemento: elemento.serialize(), allplanets))
+    # esto es un mapeo en python
+    print(allplanets)
+    return jsonify({"resultado": allplanets})
+
+
+@app.route('/user', methods=['GET'])
+def get_user():
+    # conlo siguiente consigo todos los personajes: 
+    alluser = User.query.all()
+    alluser = list(map(lambda elemento: elemento.serialize(), alluser))
+    # esto es un mapeo en python
+    print(alluser)
+    return jsonify({"resultado": alluser})
+
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+# CÓMO CONSEGUIR DESDE LA URL DE LA WEB  UN PERSONAJE AÑADIDO O PLANETA AÑADIDO COLOCANDO /PEOPLE/1  Y /PLANETS/2  X EJEMPLO Y ME SALE LA SERIALIZE() CON LOS DATOS
+@app.route('/people/<int:id>', methods=['GET'])
+# esta parte es para buscar un personaje agregado de mi lista desde la url añadiendo /people/1 --> y me sale luke skwalkler
+def get_one_people(id):
+    # bajo un parametro especifico
+    # query = consultar"
+    onepeople = People.query.filter_by(id=id).first()
+    # .firts()... me devuelve el primer elemento 
+    # .all() me devuelve un array 
+    # onepeople = People.query.filter_by(gender = n/a).first()
+    onepeople = People.query.get(id).serialize()
+
+    return jsonify({"resultado": onepeople})
+   
+    # buscar solo por el id
+
+
+@app.route('/planets/<int:id>', methods=['GET'])
+# esta parte es para buscar un personaje agregado de mi lista desde la url añadiendo /people/1 --> y me sale luke skwalkler
+def get_one_planets(id):
+    # bajo un parametro especifico
+    # query = consultar"
+# 1 MANERA DE HACERLA
+    # oneplanets = Planets.query.filter_by(id=id).first()
+
+    # .firts()... me devuelve el primer elemento 
+    # .all() me devuelve un array 
+# 2 MANERA DE HACERLO MÁS SENCILLO
+    # onepeople = People.query.filter_by(gender = n/a).first()
+    oneplanets = Planets.query.get(id).serialize()
+
+    return jsonify({"resultado": oneplanets})
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+# comandos: 
+# pipenv run migrate ..chequea los cambios
+# pipenv 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,11 +12,95 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(120), unique=False, nullable=True)
+    # en el gender, el dato no tiene porqué ser unico porque pueden haber mas hombres o mas mujeres y se puede repetir por eso ponemos false
+    hair_color = db.Column(db.String(120), unique=False, nullable=True)
+    # puede quedars el campo nulo, vacío el nullable 
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            # do not serialize the password, its a security breach
+        }
+
+class Fav_people(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # quien le dio a favorito
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # quien es el favorito
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    rel_user = db.relationship(User)
+    rel_people = db.relationship(People)
+
+    def __repr__(self):
+        return '<FavPeople %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=True)
+    terrain = db.Column(db.String(120), unique=False, nullable=True)
+    # en el gender, el dato no tiene porqué ser unico porque pueden haber mas hombres o mas mujeres y se puede repetir por eso ponemos false
+    climate = db.Column(db.String(120), unique=False, nullable=True)
+    # puede quedars el campo nulo, vacío el nullable 
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Fav_planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # quien le dio a favorito
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # quien es el favorito
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    rel_user = db.relationship(User)
+    rel_people = db.relationship(Planets)
+
+    def __repr__(self):
+        return '<FavPlanets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planets_id": self.planets_id,
             # do not serialize the password, its a security breach
         }
